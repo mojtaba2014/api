f20a3a9e1c98208a3de2f477824a3b2ecc30ecaf
Mojtaba.najaffi <najafi_2011mn@yahoo.com>
2018-11-14 23:20:16 +0100

    Update equityController.cs

--- equityController.cs
+++ equityController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Web.Http;
 using WebApi.Models;
 
@@ -14,22 +16,72 @@ namespace WebApi.Controllers
 {
     public class equityController : ApiController
     {
- 
- 
-       
-            equity[] equities = new equity[]
-            {
-            new equity { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new equity  { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new equity  { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-            };
+
+         
 
             public IEnumerable<equity> GetAllProducts()
             {
+
+
+            private const int PORT = 12345;
+
+
+        string data;
+        IPEndPoint serverAddress;
+        NetworkStream networkstream;
+        StreamReader streamreader;
+        StreamWriter streamwriter;
+        string input;
+
+        serverAdress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT);
+
+                Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                                                      ProtocolType.Tcp);
+                   server.Connect(serverAddress);
+                
+        
+
+networkstream = new NetworkStream(server);
+streamreader = new StreamReader(networkstream);
+streamwriter = new StreamWriter(networkstream);
+
+var data = streamreader.ReadLine();         // Read welcome from Server
+                                            // Console.WriteLine(data);
+
+        List<equity> equities = new  List<equity>();
+
+                while (true)                // While communicating with Server
+                {
+                    input = Console.ReadLine();         // Read text from Console
+                    if (input == "exit")                // Close connection to Server?
+                        break;
+                    //streamwriter.WriteLine(input);      // Send text to Server
+                    streamwriter.Flush();
+
+                    data = streamreader.ReadLine();     // Read echo from Server
+                    equintities. Add(data)           // Write echo to Console
+                }
+
+                Console.WriteLine("Closing connection to Server...");
+                streamreader.Close();
+                streamwriter.Close();
+                networkstream.Close();
+                server.Shutdown(SocketShutdown.Both);
+                server.Close();
+
+            
+    
+
+        //equity[] equities = new equity[]
+        //    {
+        //    new equity { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+        //    new equity  { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+        //    new equity  { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        //    };
                 return equities;
             }
-
-            public IHttpActionResult GetProduct(int id)
+               
+            public IHttpActionResult saveProductinDB(int id)
             {
                 var product = equities.FirstOrDefault((p) => p.Id == id);
             if (product == null)
@@ -44,7 +96,7 @@ namespace WebApi.Controllers
 
                 for (int i = 0; i < equities.Length; i++)
                 {
-                    SqlCommand command = new SqlCommand("insert into equity " + "values('" + equities[i].ToString() + "', '')"+"order by name", sql);
+                    SqlCommand command = new SqlCommand("insert into equity " + "values('" + equities[i].ToString() + "', '')" , sql);
                     command.ExecuteNonQuery();
                 }
                 sql.Close();
@@ -52,7 +104,4 @@ namespace WebApi.Controllers
             }
             return Ok(product);
             }
-        }
-    }
-}
-}
+ 
