e1c3ba7293e3b87df31827bff7ed7dfc04eb9b1e
Mojtaba.najaffi <najafi_2011mn@yahoo.com>
2019-02-06 09:43:46 +0100

    Add files via upload

---
+++ DBReturnValues.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolekClient.Model
+{
+    public class DBReturnValues
+    {
+        public List<List<string>> Rows;
+        public List<string> ColumnNames;
+        public List<string> Types;
+
+        public string Result { get; set; }
+        public DBReturnValues()
+        {
+            Rows = new List<List<string>>();
+            ColumnNames = new List<string>();
+            Types = new List<string>();
+        }
+    }
+}
