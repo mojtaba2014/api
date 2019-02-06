58d1f925f6a2e089867b8c628b81d6a5dde2fab9
Mojtaba.najaffi <najafi_2011mn@yahoo.com>
2019-02-06 09:44:55 +0100

    Add files via upload

---
+++ ServerClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolekClient.Model
+{
+    public class ServerClass
+    {
+        public string ServerName { get; set; }
+
+        public ConnectionString connectionString { get; set; }
+
+        public ServerClass(string ServerName, ConnectionString connectionString)
+        {
+            this.ServerName = ServerName;
+            this.connectionString = connectionString;
+        }
+    }
+}
