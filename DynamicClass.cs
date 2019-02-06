8ebafd6eac411f0a455900afd400907ac71a93ad
Mojtaba.najaffi <najafi_2011mn@yahoo.com>
2019-02-06 09:44:24 +0100

    Add files via upload

---
+++ DynamicClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolekClient.Model
+{
+    public class DynamicClass
+    {
+        public string Type { get; set; }
+
+        public string Value { get; set; }
+
+        public string Name { get; set; }
+
+        public DynamicClass(string Value, string Type, string Names)
+        {
+            this.Value = Value;
+            this.Type = Type;
+            this.Name = Names;
+        }
+
+        public DynamicClass()
+        {
+
+        }
+    }
+}
