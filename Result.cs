9f29c5e90dac78f4b02acf13ed69826c2d54bda3
Mojtaba.najaffi <najafi_2011mn@yahoo.com>
2019-02-06 09:44:41 +0100

    Add files via upload

---
+++ Result.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolekClient.Model
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public System.DateTime DATETIME { get; set; }
+        public System.Guid GUID { get; set; }
+        public string SG10IG001IN0001 { get; set; }
+        public int SG10IG001IN0002 { get; set; }
+        public int SG10IG001IN0003 { get; set; }
+        public int SG10IG002IN0001 { get; set; }
+        public int SG10IG002IN0002 { get; set; }
+        public int SG10IG002IN0006 { get; set; }
+        public int SG10IG002IN0007 { get; set; }
+        public int SG10IG003IN0002 { get; set; }
+        public int SG10IG003IN0003 { get; set; }
+        public int SG10IG005IN0001 { get; set; }
+        public int SG10IG005IN0002 { get; set; }
+        public int SG10IG005IN0003 { get; set; }
+        public int SG10IG010IN0001 { get; set; }
+        public int SG10IG010IN0002 { get; set; }
+        public int SG10IG010IN0003 { get; set; }
+        public int SG10IG010IN0004 { get; set; }
+        public int SG10IG010IN0005 { get; set; }
+        public int SG10IG010IN0006 { get; set; }
+        public int SG10IG010IN0007 { get; set; }
+        public int SG10IG010IN0010 { get; set; }
+        public int SG10IG010IN0012 { get; set; }
+        public int SG10IG010IN0013 { get; set; }
+        public int SG10IG010IN0014 { get; set; }
+        public int SG10IG010IN0015 { get; set; }
+        public int SG10IG010IN0016 { get; set; }
+        public int SG10IG010IN0017 { get; set; }
+        public int SG10IG050IN0001 { get; set; }
+        public int SG10IG050IN0002 { get; set; }
+        public int SG10IG050IN0009 { get; set; }
+        public float SG10IG050IN0010 { get; set; }
+        public int SG10IG051IN0001 { get; set; }
+        public int SG10IG051IN0002 { get; set; }
+        public float SG10IG051IN0010 { get; set; }
+        public float SG10IG051IN0011 { get; set; }
+        public double SG10IG051IN0012 { get; set; }
+        public float SG10IG051IN0013 { get; set; }
+        public double SG10IG051IN0014 { get; set; }
+        public float SG10IG051IN0015 { get; set; }
+        public double SG10IG051IN0016 { get; set; }
+        public float SG10IG051IN0017 { get; set; }
+        public double SG10IG051IN0018 { get; set; }
+        public int SG10IG052IN0001 { get; set; }
+        public int SG10IG052IN0002 { get; set; }
+        public int SG10IG052IN0010 { get; set; }
+        public float SG10IG052IN0011 { get; set; }
+        public int SG10IG053IN0001 { get; set; }
+        public int SG10IG053IN0002 { get; set; }
+        public int SG10IG053IN0015 { get; set; }
+        public int SG10IG053IN0016 { get; set; }
+        public int SG10IG054IN0001 { get; set; }
+        public int SG10IG054IN0002 { get; set; }
+        public float SG10IG054IN0010 { get; set; }
+        public int SG10IG055IN0001 { get; set; }
+        public int SG10IG055IN0002 { get; set; }
+        public float SG10IG055IN0010 { get; set; }
+        public int SG10IG055IN0011 { get; set; }
+        public int SG10IG055IN0016 { get; set; }
+        public int SG10IG055IN0017 { get; set; }
+        public int SG10IG055IN0018 { get; set; }
+        public int SG10IG056IN0001 { get; set; }
+        public int SG10IG056IN0002 { get; set; }
+        public float SG10IG056IN0010 { get; set; }
+        public int SG10IG056IN0011 { get; set; }
+        public int SG10IG056IN0013 { get; set; }
+        public int SG10IG056IN0017 { get; set; }
+        public int SG10IG056IN0018 { get; set; }
+        public int SG10IG057IN0001 { get; set; }
+        public int SG10IG057IN0002 { get; set; }
+        public float SG10IG057IN0010 { get; set; }
+        public int SG10IG057IN0011 { get; set; }
+        public int SG10IG057IN0014 { get; set; }
+        public int SG10IG057IN0020 { get; set; }
+        public int SG10IG057IN0021 { get; set; }
+        public int SG10IG058IN0001 { get; set; }
+        public int SG10IG058IN0002 { get; set; }
+        public float SG10IG058IN0010 { get; set; }
+        public int SG10IG058IN0011 { get; set; }
+        public float SG10IG058IN0014 { get; set; }
+        public int SG10IG058IN0020 { get; set; }
+        public int SG10IG058IN0021 { get; set; }
+        public int SG10IG059IN0001 { get; set; }
+        public int SG10IG059IN0002 { get; set; }
+        public int SG10IG059IN0010 { get; set; }
+        public int SG10IG059IN0011 { get; set; }
+        public int SG10IG060IN0001 { get; set; }
+        public int SG10IG060IN0002 { get; set; }
+        public float SG10IG060IN0010 { get; set; }
+        public int SG10IG061IN0001 { get; set; }
+        public int SG10IG061IN0002 { get; set; }
+        public int SG10IG061IN0010 { get; set; }
+        public int SG10IG061IN0011 { get; set; }
+        public int SG10IG061IN0016 { get; set; }
+        public int SG10IG061IN0017 { get; set; }
+        public int SG10IG061IN0018 { get; set; }
+        public int SG10IG062IN0001 { get; set; }
+        public int SG10IG062IN0002 { get; set; }
+        public int SG10IG062IN0010 { get; set; }
+        public float SG10IG062IN0011 { get; set; }
+        public int SG10IG062IN0013 { get; set; }
+        public int SG10IG062IN0017 { get; set; }
+        public int SG10IG062IN0018 { get; set; }
+        public int SG10IG063IN0001 { get; set; }
+        public int SG10IG063IN0002 { get; set; }
+        public float SG10IG063IN0010 { get; set; }
+        public int SG10IG063IN0011 { get; set; }
+        public float SG10IG063IN0014 { get; set; }
+        public int SG10IG063IN0020 { get; set; }
+        public int SG10IG063IN0021 { get; set; }
+        public int SG10IG064IN0001 { get; set; }
+        public int SG10IG064IN0002 { get; set; }
+        public float SG10IG064IN0010 { get; set; }
+        public int SG10IG064IN0011 { get; set; }
+        public float SG10IG064IN0014 { get; set; }
+        public int SG10IG064IN0020 { get; set; }
+        public int SG10IG064IN0021 { get; set; }
+        public int SG10IG065IN0001 { get; set; }
+        public int SG10IG065IN0002 { get; set; }
+        public int SG10IG066IN0001 { get; set; }
+        public int SG10IG066IN0002 { get; set; }
+
+        public Result()
+        {
+
+        }
+    }
+}
