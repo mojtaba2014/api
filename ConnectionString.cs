934a85dd67075d0f7adaa6e85a500cf5d0555b7c
Mojtaba.najaffi <najafi_2011mn@yahoo.com>
2019-02-06 09:43:13 +0100

    Add files via upload

---
+++ ConnectionString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolekClient.Model
+{
+    public class ConnectionString
+    {
+
+        public string Database { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+
+        public ConnectionString()
+        {
+
+        }
+
+        public ConnectionString(string Database, string UserName, string PassWord)
+        {
+            this.Database = Database;
+            this.UserName = UserName;
+            this.PassWord = PassWord;
+        }
+    }
+}
