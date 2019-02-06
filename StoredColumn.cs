2b9ae8cfdbbbc880195003513aeb98f71cc53a7f
Mojtaba.najaffi <najafi_2011mn@yahoo.com>
2019-02-06 09:45:12 +0100

    Add files via upload

---
+++ StoredColumn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolekClient.Model
+{
+    public class StoredColumn
+    {
+        public string Name { get; set; }
+        public List<string> ColumnNames { get; set; }
+        public List<string> ColumnTypes { get; set; }
+
+        public StoredColumn()
+        {
+            ColumnNames = new List<string>();
+            ColumnTypes = new List<string>();
+        }
+    }
+}
